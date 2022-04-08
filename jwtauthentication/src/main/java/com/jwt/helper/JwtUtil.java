28061d8a3add3157e92fba2d40c0be451bc17338
Pradeep Nishad <101691906+pradeep354@users.noreply.github.com>
2022-04-08 14:28:23 +0530

    first commit

---
+++ jwtauthentication/src/main/java/com/jwt/helper/JwtUtil.java
@@ -0,0 +1,71 @@
+package com.jwt.helper;
+
+//methods-for generating token
+//validate-tokens
+//isexpired
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtUtil {
+	private static final long serialVersionUID=-2550185165626007488L;
+	public static final long JWT_TOKEN_VALIDITY= 5 * 60 * 60;
+
+    private String secret= "java";
+
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+    
+    //for retrieving any information from token we will need the secret key
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+    //check if the token has expired
+
+   private Boolean isTokenExpired(String  token)
+    {
+	   return extractExpiration(token).before(new Date());
+    }
+    //generate token from users
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return doGenerateToken(claims, userDetails.getUsername());
+    }
+    //while creating the tokens
+    //1.define claims of the tokens,likeIssuer,Expiration,Subject and ID
+    //2.Sign the jwt using HS512 algorithm and secret key.
+    //2.Acording to the compact serialization 
+
+    private String doGenerateToken(Map<String, Object> claims, String subject) {
+
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY*1000))
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+    }
+//validate the token
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+}
